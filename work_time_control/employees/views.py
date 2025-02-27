5bdb0a3c76f12856f610cc312606055e4e19d3eb
mixma5ter <deejay999@yandex.ru>
2025-02-27 14:27:24 +0300

    index template added

--- work_time_control/employees/views.py
+++ work_time_control/employees/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.shortcuts import render
 from rest_framework import generics
 
 from .models import Employee
@@ -14,3 +16,30 @@ class EmployeeDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = EmployeeSerializer
     lookup_field = 'card_number'
     # permission_classes = [permissions.IsAuthenticated]
+
+
+def index(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        try:
+            last_employee = Employee.objects.latest('last_checkin')
+            data = {
+                'name': last_employee.employee_name,
+                'card_number': last_employee.card_number,
+                'time': last_employee.last_checkin.strftime('%Y-%m-%d %H:%M:%S'),
+                'is_active': last_employee.is_active,
+            }
+            return JsonResponse(data)
+        except Employee.DoesNotExist:
+            return JsonResponse({'message': 'No checkins yet'})
+    else:
+        try:
+            last_employee = Employee.objects.latest('last_checkin')
+            context = {
+                'name': last_employee.employee_name,
+                'card_number': last_employee.card_number,
+                'time': last_employee.last_checkin.strftime('%Y-%m-%d %H:%M:%S'),
+                'is_active': last_employee.is_active,
+            }
+        except Employee.DoesNotExist:
+            context = {'message': 'No checkins yet'}
+        return render(request, 'employees/index.html', context)
--- work_time_control/work_time_control/settings.py
+++ work_time_control/work_time_control/settings.py
@@ -16,7 +16,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # IP адреса, при обращении с которых будет доступен DjDT
 INTERNAL_IPS = ['127.0.0.1']
