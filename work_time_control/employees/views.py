7ccc06a066c3d5ec4a12f68ba444e25eb9d1fd91
mixma5ter <deejay999@yandex.ru>
2025-03-13 10:41:49 +0300

    add style

--- work_time_control/employees/views.py
+++ work_time_control/employees/views.py
@@ -1,5 +1,8 @@
+import locale
+
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework import generics
 
 from .models import Employee
@@ -19,21 +22,34 @@ class EmployeeDetailView(generics.RetrieveUpdateAPIView):
 
 
 def index(request):
+    locale.setlocale(locale.LC_TIME, 'ru_RU')
+
+    # Обработка AJAX запросов
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         try:
             last_employee = Employee.objects.latest('last_checkin')
+            formatted_last_checkin = timezone.localtime(last_employee.last_checkin).strftime("%d %B %Y г., %H:%M")
+
             data = {
-                'name': last_employee.employee_name,
+                'card_number': last_employee.card_number,
+                'employee_name': last_employee.employee_name,
+                'last_checkin': formatted_last_checkin,
                 'is_active': last_employee.is_active,
             }
             return JsonResponse(data)
         except Employee.DoesNotExist:
             return JsonResponse({'message': 'No checkins yet'})
+
+    # Обработка обычных запросов (не AJAX)
     else:
         try:
             last_employee = Employee.objects.latest('last_checkin')
+            formatted_last_checkin = timezone.localtime(last_employee.last_checkin).strftime("%d %B %Y г., %H:%M")
+
             context = {
-                'name': last_employee.employee_name,
+                'card_number': last_employee.card_number,
+                'employee_name': last_employee.employee_name,
+                'last_checkin': formatted_last_checkin,
                 'is_active': last_employee.is_active,
             }
         except Employee.DoesNotExist:
